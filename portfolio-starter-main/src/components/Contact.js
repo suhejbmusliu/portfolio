084751bad75935ee24ab460f74e02614e717d063
suhejbmusliu <suhejbmusliu69@gmail.com>
2024-06-04 02:23:11 +0200

    update

--- portfolio-starter-main/src/components/Contact.js
+++ portfolio-starter-main/src/components/Contact.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import emailjs from 'emailjs-com'; // Import EmailJS library
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
-  const sendEmail = (e) => {
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
     e.preventDefault();
 
-      // Accessing user's email from the form
-  const userEmail = e.target.email.value;
+    console.log(formData.email); 
 
-  // Replace these values with your EmailJS service ID, template ID, and user ID
-  emailjs.sendForm('service_xax1j09', 'template_ellrm2l', e.target, 'R0rzN9Wn-2VS0-lpc')
-    .then((result) => {
-      console.log(result.text);
-      window.alert("Email sent successfully!"); // Alert for successful email sending
-      // Add any success message or redirection logic here
-    }, (error) => {
-      console.log(error.text);
-      window.alert("Failed to send email. Please try again later."); // Alert for failed email sending
-      // Add any error handling logic here
+    emailjs.sendForm('service_xax1j09', 'template_dra9wh4', e.target, 'R0rzN9Wn-2VS0-lpc')
+      .then((result) => {
+        console.log(result.text);
+        
+      }, (error) => {
+        console.log(error.text);
+ 
+      });
+
+  
+    setFormData({
+      name: '',
+      email: '',
+      message: ''
     });
   };
 
@@ -37,12 +49,14 @@ const Contact = () => {
           </motion.div>
 
           {/* form */}
-          <motion.form onSubmit={sendEmail} variants={fadeIn('left', 0.3)} initial="hidden" whileInView={"show"} viewport={{once: false, amount: 0.3}} className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 mb-40 items-start'>
+          <motion.form onSubmit={handleSubmit} variants={fadeIn('left', 0.3)} initial="hidden" whileInView={"show"} viewport={{once: false, amount: 0.3}} className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 mb-40 items-start'>
 
             <input 
               className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
               type='text'
               name='name'
+              value={formData.name}
+              onChange={handleChange}
               placeholder='Your name'
             />
 
@@ -50,16 +64,20 @@ const Contact = () => {
               className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
               type='email'
               name='email'
+              value={formData.email}
+              onChange={handleChange}
               placeholder='Your email'
             />
             
             <textarea 
               className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12'
               name='message'
+              value={formData.message}
+              onChange={handleChange}
               placeholder='Your message'
             ></textarea>
 
-            <button type='submit' className='btn btn-lg'>
+            <button type="submit" className='btn btn-lg'>
               Send message
             </button>
           </motion.form>
